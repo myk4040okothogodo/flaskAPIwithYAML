3b5a417dd06a8093e488ed6bb4a7ba01f5ded0d7
myk4040okothogodo <mikeogodo5@gmail.com>
2022-01-06 23:01:28 +0300

    inception

---
+++ models.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+from config import db, ma
+from marshmallow_sqlalchemy import ModelSchema
+
+class Note(db.Model):
+    __tablename__ = 'note'
+    note_id = db.Column(db.Integer, primary_key=True)
+    person_id =db.Column(db.Integer, db.ForeignKey('person.person_id'))
+    content = db.Column(db.String, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+
+
+class Person(db.Model):
+    __tablename__ = 'person'
+    person_id = db.Column(db.Integer, primary_key=True)
+    lname = db.Column(db.String(32), index=True)
+    fname = db.Column(db.String(32))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    notes = db.relationship(
+        'Note',
+        backref = 'person',
+        cascade = 'all, delete, delete-orphan',
+        single_parent= True,
+        order_by = 'desc(Note.timestamp)'
+    )   
+class PersonSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Person
+        load_instance = True
+        sqla_session = db.session
+    notes = fields.Nested('PersonNoteSchema', default=[],many=True)
+            
+
+class PersonNoteSchema(ma.SQLAlchemyAutoSchema):
+    """
+    This class exists to get around a recursion issue
+    """
+    note_id = fields.Int()
+    person_id = fields.Int()
+    content = fields.Str()
+    timestamp = fields.Str()
+    
+class NoteSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Note
+        load_instance = True
+        sqla_session = db.session
+    person = fields.Nested('NotePersonSchema', default=None)
+
+class NotePersonSchema(ma.SQLAlchemyAutoSchema):
+    """
+    This class exists to get around a recursion issue
+    """
+    person_id = fields.Int()
+    lname = fields.Str()
+    fname = fields.Str()
+    timestamp = fields.Str()            
